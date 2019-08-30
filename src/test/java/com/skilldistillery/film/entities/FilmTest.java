b726017d4f610e4a25b2d742c6ac28c3a1562866
JHowell78 <jahowell1978@gmail.com>
2019-08-30 15:31:30 -0600

    First commit

---
+++ src/test/java/com/skilldistillery/film/entities/FilmTest.java
@@ -0,0 +1,30 @@
+package com.skilldistillery.film.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FilmTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void teardownAfterClass() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
